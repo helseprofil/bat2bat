7e55a827b5ae23d9ebffaf4fe87ff64e1ba98641
Yusman Kamaleri <ybkamaleri@gmail.com>
2022-02-17 17:44:57 +0100

    Doc: Update logo

--- dev/logo.R
+++ dev/logo.R
@@ -5,7 +5,7 @@ library("hexSticker")
 
 imgFolder <- "/home/ybk/Git-fhi/bat2bat/dev"
 img <- file.path(imgFolder, "bat2bat.png")
-sticker(img, package = "", s_x = .95, s_width = .7,
+sticker(img, package = "", s_x = .95, s_width = .55,
         h_color = "#000080", #border
-        h_fill = "#6495ED",
-        filename = file.path(imgFolder, "logo.png"))
+        h_fill = "#ffe0bd",
+        filename = file.path(imgFolder, "logo2.png"))
